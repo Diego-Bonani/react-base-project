776459cb66a85b9a5380de77350a12dea8922437
DIEGO GODE BONANI <202401000711@alunos.ibmec.edu.br>
2024-10-03 17:02:05 -0300

    Legal

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,18 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+import Base from "./Base";
+
+const Nova = () => {
+       
+    return (
+    <div>
+        <Base />
+        <h1>
+        Primeira Página Web React do Diego
+        </h1>
+        <p>ooiiii</p>
+        <NovoComponente />
+    </div>
+  );
+}
+
+export default Nova;
